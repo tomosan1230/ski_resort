ee25d59de22d3f7cdb9af0832f1f98c63dd797f0
EC2 Default User <ec2-user@ip-172-31-21-68.ap-northeast-1.compute.internal>
2022-12-09 05:42:50 +0000

    [Finish]機能実装

--- app/controllers/admin/prefectures_controller.rb
+++ app/controllers/admin/prefectures_controller.rb
@@ -1,7 +1,38 @@
 class Admin::PrefecturesController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
+    @prefectures = Prefecture.all
+    @prefecture =Prefecture.new
+  end
+
+  def create
+    @prefecture = Prefecture.new(prefecture_params)
+    if @prefecture.save
+      redirect_to admin_prefectures_path, notice: '内容を保存しました'
+    else
+      @prefectures = Prefecture.all
+      render :index
+    end
   end
 
   def edit
+   @prefecture = Prefecture.find(params[:id])
   end
+
+  def update
+    @prefecture = Prefecture.find(params[:id])
+    if @prefecture.update(prefecture_params)
+      redirect_to admin_prefectures_path, notice: '内容を保存しました'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def prefecture_params
+    params.require(:prefecture).permit(:name)
+  end
+
 end
