312bb3b22f2fad087c67659eaf6eb50eba30a496
EC2 Default User <ec2-user@ip-172-31-21-68.ap-northeast-1.compute.internal>
2022-12-10 12:26:47 +0000

    [Update]Admin/member prefecture

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,6 +1,6 @@
 class Admin::MembersController < ApplicationController
   def index
-    @members = Member.all
+    @members = Member.page(params[:page]).per(1)
   end
 
   def show
@@ -10,20 +10,22 @@ class Admin::MembersController < ApplicationController
   def edit
     @member = Member.find(params[:id])
   end
-  
+
   def update
     @member = Member.find(params[:id])
     if @member.update(member_params)
       redirect_to admin_member_path, notice: "内容を保存しました"
     else
+      flash[:notice] = "保存できませんでした"
+      @member = Member.find(params[:id])
       render :edit
     end
   end
-  
+
   private
-  
+
   def member_params
     params.require(:member).permit(:nick_name, :email, :is_deleted)
   end
-  
+
 end
