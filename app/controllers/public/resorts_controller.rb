518669997e34a9040794854ff4a5910a4fb6217e
EC2 Default User <ec2-user@ip-172-31-21-68.ap-northeast-1.compute.internal>
2022-12-10 02:52:04 +0000

    [Add]Public/resort

---
+++ app/controllers/public/resorts_controller.rb
@@ -0,0 +1,18 @@
+class Public::ResortsController < ApplicationController
+  def index
+    @resort = Resort.all
+    @prefectures = Prefecture.all
+  end
+
+  def show
+    @resort = Resort.find(params[:id])
+    @reviews = Review.all
+  end
+
+  private
+
+  def resort_params
+    params.require(:resort).permit(:name, :image_id, :feature, :postal_code, :address)
+  end
+
+end
