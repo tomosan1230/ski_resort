6180a56fbf8b82d1cc6dfd4897533ef22e03b975
EC2 Default User <ec2-user@ip-172-31-21-68.ap-northeast-1.compute.internal>
2022-12-25 04:21:13 +0000

    [Update]

--- app/models/review.rb
+++ app/models/review.rb
@@ -10,7 +10,7 @@ class Review < ApplicationRecord
     less_than_or_equal_to: 5,
     greater_than_or_equal_to: 0.5
   }, presence: true
-  
+
   scope :latest, -> {order(created_at: :desc)}
   scope :old, -> {order(created_at: :asc)}
   scope :rate_count, -> {order(rate: :desc)}
